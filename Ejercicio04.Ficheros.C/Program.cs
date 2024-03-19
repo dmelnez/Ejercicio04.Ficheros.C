6adc195596b2c35964ede54f0ee6bae931faaf90
DESKTOP-JV3V0TT\dmelnez <csi23-dmelnez@colegioaltair.net>
2024-03-19 14:08:48 +0100

    Agregar archivos de proyecto.

---
+++ Ejercicio04.Ficheros.C/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Ejercicio04.Ficheros.C
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                string rutaFichero = "C:\\Users\\dmelnez\\Desktop\\ActividadEjercicioC.txt";
+
+
+                Console.WriteLine("NUMERO DE LINEA: ");
+                int numeroLinea = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("POSICION A MODIFICAR: ");
+                int posicion = Convert.ToInt32(Console.ReadLine());
+
+
+                Console.WriteLine("TEXTO A REEMPLAZAR: ");
+                string textoAReemplazar = Console.ReadLine();
+
+
+                string[] lineasArchivo = File.ReadAllLines(rutaFichero);
+
+
+                   string nuevaLinea =  lineasArchivo[numeroLinea - 1].Insert(posicion, textoAReemplazar);
+
+                lineasArchivo[numeroLinea -1] = nuevaLinea;
+
+                File.WriteAllLines(rutaFichero, lineasArchivo);
+
+
+            }
+
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("[INFO] -> SE HA PODUCIDO UN ERRO");
+
+            }
+
+        }
+    }
+
+}
+
+
